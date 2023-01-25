61e34ef21a651ae294cf6d5ee994e4cf35d58c2f
DrCAO <ahjxcrz@gmail.com>
2023-01-25 11:43:34 -0800

    update all

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-ba6c34f3825ed2b1.d
@@ -0,0 +1,5 @@
+/Users/wenwang/Documents/ahjxcrz-github/LearnRust/projects/hello_cargo/target/debug/deps/hello_cargo-ba6c34f3825ed2b1: src/main.rs
+
+/Users/wenwang/Documents/ahjxcrz-github/LearnRust/projects/hello_cargo/target/debug/deps/hello_cargo-ba6c34f3825ed2b1.d: src/main.rs
+
+src/main.rs:
