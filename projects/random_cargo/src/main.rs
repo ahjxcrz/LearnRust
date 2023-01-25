dc98ea971a72e6b43c5088fc8e4a08628b2333da
DrCAO <ahjxcrz@gmail.com>
2023-01-25 14:40:54 -0800

    update all

--- projects/random_cargo/src/main.rs
+++ projects/random_cargo/src/main.rs
@@ -52,11 +52,57 @@ fn main() {
 
   let result = array_plus1(&mut a);
   println!("the result is {result}");
+
+  let mut aLEN = i32::from(a.len() as i32);
+  println!("the length is {aLEN}");
+
+  for i in 0..aLEN
+  {
+     println!("i is {i}");
+  }
+
+  // now we will call a normal print_array 
+  normal_print_array(a);
+  let mut aLEN2 = i32::from(a.len() as i32);  
+  for i in 0..aLEN2
+  {
+     println!("i is {i}");
+  }
+
+  let s = String::from("hello");  // s comes into scope
+  takes_ownership(s); 
+  //println!("string is still here? {s}"); this didn't work any more
+  let s = String::from("hello");  // s comes into scope
+  takes_ownershipv2(&s);
+  println!("string is still here? {s}");
+
+}
+
+fn takes_ownership(some_string: String) { // some_string comes into scope
+    println!("{}", some_string);
+}
+
+fn takes_ownershipv2(some_string: &String) { // some_string comes into scope
+    println!("{}", some_string);
+}
+
+fn normal_print_array(x:[i32;5])
+{
+   let aLEN = x.len(); 
+   println!("the length is ... {}",aLEN);
 }
 
 
 fn print_array(x:&mut [i32]) {
-    println!("The value of array is: {:?}",x);
+    let y2 = x;
+    for (i, item) in y2.iter().enumerate()
+    {
+       println!("i is {i}");
+       println!("item is {item}");
+    }
+
+    9;
+    /*println!("The value of array is: {:?}",x);
     x[0] = 100;
 
 
@@ -64,9 +110,13 @@ fn print_array(x:&mut [i32]) {
     {
        println!("Element: {item}");
     }
-
-
-    //let mut aLEN = x.len() as i32;
+    */
+    //let x2 = &mut x;    // can we do that?  no, you cannot, the x is moved 
+    //let aLEN  = y2.len(); 
+    //for i in x.iter_mut()
+    //{println!("i ii {i}");}
+    //let x2 = x.clone(); 
+    //let mut aLEN = i32::from(x2.len() as i32);
     //let arrayLEN:&usize = &x.len();
     //println!("the length is {}",aLEN);
     //for i in 0..aLEN
