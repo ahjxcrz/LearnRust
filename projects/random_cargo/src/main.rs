9c599f5290b73b321ac5a6a413e3faf759e68c5a
DrCAO <ahjxcrz@gmail.com>
2023-01-25 15:25:52 -0800

    update all

--- projects/random_cargo/src/main.rs
+++ projects/random_cargo/src/main.rs
@@ -76,6 +76,29 @@ fn main() {
   takes_ownershipv2(&s);
   println!("string is still here? {s}");
 
+
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s);
+
+    //s.clear(); // error!
+
+    println!("the first word is: {}", word);
+
+
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    //s.len()
+    &s[..]
 }
 
 fn takes_ownership(some_string: String) { // some_string comes into scope
