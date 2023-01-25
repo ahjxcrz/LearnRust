8bffe5dab8560363c07b113d750d31bdc7e7eeea
DrCAO <ahjxcrz@gmail.com>
2023-01-25 12:23:47 -0800

    update all

--- projects/random_cargo/src/main.rs
+++ projects/random_cargo/src/main.rs
@@ -1,8 +1,16 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 
 fn main() {
+
+  loop{
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let mut sec_s = String::new();
+    sec_s = secret_number.to_string();
+    println!("the string is : {sec_s}");
+
     println!("Hello, world!");
    
     println!("Please enter a number:");
@@ -13,4 +21,14 @@ fn main() {
          .read_line(&mut guess)
          .expect("Failed to read line");
     println!("You guessed: {guess}");
+    println!("The secret number is {secret_number}");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+  }
 }
